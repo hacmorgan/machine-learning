53c31476986580c8d57258ca2fb06d96ffd6fd18
hacmorgan <ham430@gmail.com>
2020-04-19 16:15:50 +1000

    automatic update

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -41,7 +41,7 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 
-yvec = zeros(m,10);  % expand the y vector into a matrix of row vectors
+yvec = zeros(m,numel(Theta2(:,1)));  % expand the y vector into a matrix of row vectors
 for i = 1:m
   yvec(i,y(i)) = 1;
 end
@@ -51,10 +51,11 @@ X = [ones(m,1) X];             % Add column of ones
 z1 = X * Theta1';              % calculate z1 (weighted input)
 a1 = [ones(m,1) sigmoid(z1)];  % add column of ones to sigmoid of z1
 z2 = a1 * Theta2';             % calculate z2
-h = sigmoid(z2);
+a2 = sigmoid(z2);
+h = a2;
 
 J = 1/m * sum( sum( -yvec.*log(h) - (1-yvec).*log(1-h) ) ) ...
-        + lambda/(2*m) * ( sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)) )
+    + lambda/(2*m) * ( sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)) );
 %
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
@@ -71,6 +72,11 @@ J = 1/m * sum( sum( -yvec.*log(h) - (1-yvec).*log(1-h) ) ) ...
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
+delta2 = h - yvec;
+Theta2_grad = 1/m * delta2' * a1;
+delta1 = delta2 * Theta2(:,2:end) .* sigmoidGradient(z1);
+Theta1_grad = 1/m * delta1' * X;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -79,8 +85,13 @@ J = 1/m * sum( sum( -yvec.*log(h) - (1-yvec).*log(1-h) ) ) ...
 %               and Theta2_grad from Part 2.
 %
 
+Theta2_regterm = lambda/m * Theta2;
+Theta1_regterm = lambda/m * Theta1;
+Theta2_regterm(:,1) = 0;
+Theta1_regterm(:,1) = 0;
 
-
+Theta2_grad += Theta2_regterm;
+Theta1_grad += Theta1_regterm;
 
 
 
