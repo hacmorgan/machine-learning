5faa4ae1c738d2fffdae529e8a1d2bacb2ace7c8
hacmorgan <ham430@gmail.com>
2020-05-11 05:04:21 +0000

    binarized existing data

--- turntable/image_capture/webcam_capture.py
+++ turntable/image_capture/webcam_capture.py
@@ -4,15 +4,38 @@ import cv2
 import numpy as np
 import csv
 import time
+import sys
+import argparse
+import signal
 
-start_time = time.time()
+def signal_handler(sig, frame):
+    global datafile
+    if datafile is not None:
+        datafile.close()
+    sys.exit(0)
 
-cap = cv2.VideoCapture(0)
+def get_args():    
+    parser = argparse.ArgumentParser(description='Collect training data from the webcam')
+    parser.add_argument('--outfile', '-o', type=str, help='file to write the data to')
+    return parser.parse_args()
 
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
 
-with open("../training_data/webcam.csv", "a+") as datafile:
+def main(args):
+    # Handle SIGINT and SIGTERM
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+
+    start_time = time.time()
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+
+    global datafile
+    datafile = open(args.outfile, "a+")
+
     writer = csv.writer(datafile)
     while True:
         ret, frame = cap.read()
@@ -22,12 +45,18 @@ with open("../training_data/webcam.csv", "a+") as datafile:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         downsampled = cv2.resize(gray, None, fx=0.1, fy=0.1, interpolation=cv2.INTER_AREA)
         downsampled_list = downsampled.reshape((64*48)).tolist()
+        downsampled_list.append(0)
         writer.writerow(downsampled_list)
         print("Image saved at " + str(cap_time))
         # LPs can't exceed 90 minutes, but we still want some data around the centre
         if cap_time - start_time > 92*60:
             break
         time.sleep(10)
-        
-cap.release()
-cv2.destroyAllWindows()
+
+    datafile.close()
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    sys.exit(main(get_args()))
